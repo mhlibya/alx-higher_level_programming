357125b1b76c3fd1f6efe6b36931c1dfbd5a417f
mhlibya <mhlibya728@gmail.com>
2024-03-14 18:44:07 -0700

    Gg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-counter = len(sys.argv) - 1
-if counter == 0:
-    print("0 arguments.")
-elif counter == 1:
-    print("1 argument:")
-else:
-    print("{:d} arguments:".format(counter))
-for i in range(1, counter + 1):
-    print("{:d}: {:s}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    counter = len(sys.argv) - 1
+    if counter == 0:
+        print("0 arguments.")
+    elif counter == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(counter))
+    for i in range(1, counter + 1):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
