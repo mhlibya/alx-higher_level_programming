62d80a02efd179d3c902657c9eb33fa0eabb4894
mhlibya <mhlibya728@gmail.com>
2024-03-16 17:30:11 -0700

    Gg

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     str = ""
-    for ch in my_string:
+    for ch in my_string[:]:
         if ch != 'c' and ch != 'C':
             str += ch
     return str
