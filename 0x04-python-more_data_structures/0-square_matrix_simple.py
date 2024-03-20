a47204857a384c154fa753266b4a064e515bae94
mhlibya <mhlibya728@gmail.com>
2024-03-20 15:47:36 -0700

    Gg

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 def square_matrix_simple(matrix=[]):
     square_matrix = []
-    for i in range(len(matrix)):
-        row = list(map(lambda num : num**2, matrix[i]))
+    for i in matrix:
+        row = list(map(lambda num : num**2, i))
         square_matrix.append(row)
     return square_matrix
