ea8742580cedd4b02db66d584fae4189b8350dec
mhlibya <mhlibya728@gmail.com>
2024-04-16 09:42:36 -0700

    Gg

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+================================
+shuirfgehsirougheriugheisurghgur
+================================
+"""
+class MyList(list):
+    """askldjfsdkfhlf"""
+    pass
+    def print_sorted(self):
+        """kutkuyfuyyugyig"""
+        print(sorted(list(self)))
