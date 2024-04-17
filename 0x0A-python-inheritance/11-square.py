422375aa30dd1254730e7e9fd6ed30a8d2fd75b4
mhlibya <mhlibya728@gmail.com>
2024-04-16 19:03:53 -0700

    Gg

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+======================
+   oawfyhsioeurguis
+======================
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """jflkdsliejsfn"""
+    def __init__(self, size):
+        """fasklilejzxfj"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+    def area(self):
+        """jklsdjfhef"""
+        return self.__size ** 2
+    def __str__(self):
+        """sadukfhisdgfyi"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
