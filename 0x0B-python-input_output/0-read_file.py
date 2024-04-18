680dd64b8d32696fb457d39456aefb3ca8094154
mhlibya <mhlibya728@gmail.com>
2024-04-17 18:44:08 -0700

    Gg

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-=====================
-    jfsdjfshfjehu
-=====================
-"""
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
-    """jsdklfjehfjasdf"""
-    with open(filename, encoding="UTF-8") as f:
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
