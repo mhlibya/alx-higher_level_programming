490456706f8c41df7215b187d9f0fdd60cb3ec5f
mhlibya <mhlibya728@gmail.com>
2024-06-18 16:33:09 -0700

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-argc = process.argv.length;
-if (argc == 2) {
-	console.log('No argument');
-} else if (argc == 3) {
-	console.log('Argument found');
+
+if (process.argv.length === 2) {
+	console.log('No argument')
+} else if (process.argv.length === 3) {
+	console.log('Argument found')
 } else {
-	console.log('Arguments found');
+	console.log('Arguments found')
 }
