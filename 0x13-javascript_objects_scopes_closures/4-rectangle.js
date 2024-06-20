a8a46470d65a59240fc0006ab49b0d5f88e0c7c0
mhlibya <mhlibya728@gmail.com>
2024-06-19 17:55:21 -0700

    js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,32 @@
 #!/usr/bin/node
-
 class Rectangle {
 	constructor (w, h) {
-		if (w > 0 && h > 0) {
+		if ((w > 0) && (h > 0)) {
 			this.width = w;
 			this.height = h;
 		}
 	}
-	print() {
+
+	print () {
 		for (let i = 0; i < this.height; i++) {
+			let s = '';
 			for (let j = 0; j < this.width; j++) {
-				process.stdout.write('x');
+				s += 'X';
 			}
-			console.log();
+			console.log(s);
 		}
 	}
-	rotate() {
-		let tmp = this.width;
+
+	rotate () {
+		const aux = this.width;
 		this.width = this.height;
-		this.height = tmp;
+		this.height = aux;
 	}
-	double() {
+
+	double () {
 		this.width *= 2;
 		this.height *= 2;
 	}
 }
+
 module.exports = Rectangle;
