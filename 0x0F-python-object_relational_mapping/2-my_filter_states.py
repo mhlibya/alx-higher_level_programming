d27b7374be132921a5924b652fd49aa260aa7159
mhlibya <mhlibya728@gmail.com>
2024-06-22 08:19:47 +0000

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""Script to search for states by name"""
+
 import sys
+import MySQLdb
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+def search_states(username, password, db_name, state_name):
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: ./search_states.py username password db_name state_name")
+        sys.exit(1)
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    
+    search_states(username, password, db_name, state_name)
