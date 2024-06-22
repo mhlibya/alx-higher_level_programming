32a9f89978983219b68a7c13157335ae364f23b7
mhlibya <mhlibya728@gmail.com>
2024-06-22 08:07:54 +0000

    ORM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""my filter states"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+	db = MySQLdb.connect(
+	host="localhost",
+	port=3306,
+	user=sys.argv[1],
+	passwd=sys.argv[2],
+	db=sys.argv[3]
+	)
+	cur = db.cursor()
+	cursor.execute("SELECT {} FROM states".format(sys.argv[4]))
+	rows = cur.fetchall()
+	for row in rows:
+		print(row)
+	cur.close()
+	db.close()
