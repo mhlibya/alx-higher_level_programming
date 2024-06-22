2c2cc8ca26c58825f44ec94050c4d31e495aee26
mhlibya <mhlibya728@gmail.com>
2024-06-22 09:07:43 +0000

    ORM

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""cities by state"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    rows = fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
