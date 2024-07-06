4369951a52285ec9dc02a6f661ae87a2e8e324f0
mhlibya <mhlibya728@gmail.com>
2024-07-05 17:33:38 -0700

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-# displays the size of the body of the response
-curl -sI $"1" | wc -c
+#!/bin/bash
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
