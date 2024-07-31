2e500237745b1c0a77628368574bf95060c6af0e
mhlibya <mhlibya728@gmail.com>
2024-07-31 12:48:17 +0200

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // A script that reads and prints the content of a file
 
-const file = process.argv[2];
 const fs = require('fs');
+const file = process.argv[2];
 
 fs.readFile(file, 'utf8', (error, data) => {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else {
     console.log(data);
   }
